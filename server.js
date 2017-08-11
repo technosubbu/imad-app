2a0839c37318da5956e72ba87594e25de1fe706f
technosubbu <ssiva134@gmail.com>
2017-08-11 21:54:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,19 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-
-
 app.get('/article-two',function(req,res){
-res.send('Hi check this out article two is loaded');
+res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/article-three',function(req,res){
-res.send(' Hey man article three is loaded');
+app.get('/article-two',function(req,res){
+res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 

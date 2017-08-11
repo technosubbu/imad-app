1322c7d407c6db2854072cd2918d18d41f38c268
technosubbu <ssiva134@gmail.com>
2017-08-11 23:01:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,9 +22,8 @@ var articleOne={
 };
 function createTemplate(data){
             var title= data.title;
-             
             var date=data.date;
-           var heading=data.heading;
+            var heading=data.heading;
             var content=data.content;
 
     var htmlTemplate=`<html>
